91cc1c2709d8e776b96defc94a4c061c14958935
Phil Quintanilla <pvquintanilla@gmail.com>
2021-01-10 16:04:36 -0700

    finished app.py and started index.html

---
+++ Mission_to_Mars/app.py
@@ -0,0 +1,27 @@
+#Dependencies
+from flask import Flask, render_template, redirect
+from flask_pymongo import PyMongo
+import scrape_mars
+
+#Flask Setup
+app = Flask(__name__)
+
+#mongo connection
+mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
+
+#Route
+@app.route("/")
+def index():
+    mars_stuff = mongo.db.mars_stuff.find_one()
+    return render_template("index.html", mars_stuff=mars_stuff)
+
+#Scrape Route
+@app.route("/scrape")
+def scraper():
+    mars_stuff = mongo.db.mars_stuff
+    mars_data = scrape_mars.scrape()
+    mars_stuff.update({}, mars_data, upsert=True)
+    return redirect("/", code=302)
+
+if __name__ == "__main__":
+    app.run(debug=True)
